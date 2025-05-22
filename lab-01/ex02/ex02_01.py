8060201ebdeecbd8aed377b6b247bec2f769f966
LamNguyen5724 <nguyenhoanglam0507@gmail.com>
2025-05-22 21:36:40 +0700

    [add] lab-01 ex01 ex02

---
+++ lab-01/ex02/ex02_01.py
@@ -0,0 +1,7 @@
+# Nhap ten va tuoi nguoi dung
+ten = input("Nhap ten: ")   
+tuoi = input("Nhap tuoi: ")     
+# in thong diep chao mung voi thong tin vua nhap
+print("Chào mừng,", ten, "! Bạn", tuoi, "tuổi.")
+ 
+ 
