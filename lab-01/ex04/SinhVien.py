6cbd97e5069f0154257b9cf4481804441f064753
LamNguyen5724 <nguyenhoanglam0507@gmail.com>
2025-05-22 23:13:28 +0700

    [add] lab-01 ex04 and .gitignore

---
+++ lab-01/ex04/SinhVien.py
@@ -0,0 +1,10 @@
+# SinhVien.py
+class SinhVien:
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
+        self._name = name
+        self._sex = sex
+        self._major = major
+        self._diemTB = diemTB
+        self._hocLuc = ""
