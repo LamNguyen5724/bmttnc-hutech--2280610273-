4b95a582143cff92411a7e5fd95bf4123fb73276
LamNguyen5724 <nguyenhoanglam0507@gmail.com>
2025-06-05 16:19:46 +0700

    03

--- README.md
+++
@@ -1,2 +0,0 @@
-# bmttnc-hutech--2280610273-
-&lt;Nguyễn_Hoàng_Lâm>_&lt;2280610273>
--- lab-01/ex02/ex02_02.py
+++
@@ -1,3 +0,0 @@
-ban_king = float(input("Nhap ban kinh hinh tron: "))
-dien_tich = 3.14 * (ban_king ** 2)
-print ("Diện tích của hình tròn là:", dien_tich)
